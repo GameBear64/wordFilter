0f9aa625fff745de93d031f09b578cadcc96a8ac
GamBar <vlpetrov02@gmail.com>
2022-04-03 22:55:31 +0300

    whitelist update

--- filter.js
+++ filter.js
@@ -1,50 +1,58 @@
-module.exports = async function filter(word, target) {
-    let {metaphone} = await import('metaphone')
-    let {stemmer} = await import('stemmer')
-    let strSim = require('string-comparison').levenshtein
-    let original = word
+module.exports = async function filter(word, target, whitelist) {
+  let { metaphone } = await import('metaphone');
+  let { stemmer } = await import('stemmer');
+  let strSim = require('string-comparison').levenshtein;
+  let original = word;
 
-    //0. remove discord emojis 
-    word = word.replace(/<a?:([a-zA-Z0-9_-]+):(\d{18})>/g, "")
+  //0. remove discord emojis
+  word = word.replace(/<a?:([a-zA-Z0-9_-]+):(\d{18})>/g, '');
 
-    //1. sanitize - remove confusable unicode characters
-    let confusablesFile = require("./confusables/confusables.json")
-    function sanitize(word, confusables) {
-        word = word.toLowerCase()
-        for (const [normalChar, array] of Object.entries(confusables)) {
-            array.forEach(element => {
-                let regex = new RegExp(element, "g")
-                if (regex.test(word)) word = word.replace(regex, normalChar)
-            });
-        }
-        return word
+  //1. sanitize - remove confusable unicode characters
+  let confusablesFile = require('./confusables/confusables.json');
+  function sanitize(word, confusables) {
+    word = word.toLowerCase();
+    for (const [normalChar, array] of Object.entries(confusables)) {
+      array.forEach(element => {
+        let regex = new RegExp(element, 'g');
+        if (regex.test(word)) word = word.replace(regex, normalChar);
+      });
     }
-    word = sanitize(word, confusablesFile);
-    if (new RegExp(target, "gi").test(word)) return `${original} matched with ${target}`
+    return word;
+  }
+  word = sanitize(word, confusablesFile);
+  if (new RegExp(target, 'gi').test(word)) return `${original} matched with ${target}`;
 
-    //2. normalize - normilize 2 types, remove symbols, remove lonely characters
-    word = word.normalize("NFKD").normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+  //2. normalize - normilize 2 types, remove symbols, remove lonely characters
+  word = word
+    .normalize('NFKD')
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '');
 
-    let veryNormalized = word
-    .replace(/[^a-zA-Z0-9 ]/gi, " ")
-    .split(/\s/g).reduce((acc, a) => a.length >= 4 ? acc + " " + a : acc + a, "")
-    .split(/\s/g).reduceRight((acc, a) => a.length >= 4 ? a + " " + acc : a + acc, "")
-    if (new RegExp(target, "gi").test(veryNormalized)) return `${original} normalized and matched with ${target}`
+  let veryNormalized = word
+    .replace(/[^a-zA-Z0-9 ]/gi, ' ')
+    .split(/\s/g)
+    .reduce((acc, a) => (a.length >= 4 ? acc + ' ' + a : acc + a), '')
+    .split(/\s/g)
+    .reduceRight((acc, a) => (a.length >= 4 ? a + ' ' + acc : a + acc), '');
+  if (new RegExp(target, 'gi').test(veryNormalized)) return `${original} normalized and matched with ${target}`;
 
-    //3. simularuty - for each
-    let each = word.split(" ")
+  //2.5 after normalization check whitelist
+  if (whitelist.includes(word)) return;
 
-    for (let i = 0; i < each.length; i++) { 
-        if (strSim.similarity(each[i], target) >= 0.75 || strSim.similarity(stemmer(each[i]), stemmer(target)) >= 0.75) return `${original} simular to ${target}`
-    }
-    
-    //4. phonetic check - for each, all
-    for (let i = 0; i < each.length; i++) { 
-        if (strSim.similarity(each[i], target) >= 0.5 && (metaphone(stemmer(each[i])) == metaphone(target) || metaphone(each[i]) == metaphone(target))) return `${original} sounds like ${target}`
-    }
+  //3. simularuty - for each
+  let each = word.split(' ');
 
-    if (metaphone(target).length > 5 && new RegExp(metaphone(target), "gi").test(metaphone(word))) return `${original} has a sound like ${target}`
+  for (let i = 0; i < each.length; i++) {
+    if (strSim.similarity(each[i], target) >= 0.75 || strSim.similarity(stemmer(each[i]), stemmer(target)) >= 0.75) return `${original} simular to ${target}`;
+  }
 
-    //otherwise return ok
-    return false
-}
+  //4. phonetic check - for each, all
+  for (let i = 0; i < each.length; i++) {
+    if (strSim.similarity(each[i], target) >= 0.5 && (metaphone(stemmer(each[i])) == metaphone(target) || metaphone(each[i]) == metaphone(target))) return `${original} sounds like ${target}`;
+  }
+
+  if (metaphone(target).length > 5 && new RegExp(metaphone(target), 'gi').test(metaphone(word))) return `${original} has a sound like ${target}`;
+
+  //otherwise return ok
+  return false;
+};
--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-let filter = require("./filter.js")
+let filter = require('./filter.js');
 
-let target = "niagara"
+let target = 'niagara';
+
+let whitelist = ['neagara'];
 
 //positive results
-filter("🇳ɪa🇬ara", target)
-.then(result => {if (result) console.log(result)}) // unicode example
+filter('🇳ɪa🇬ara', target, whitelist).then(result => {
+  if (result) console.log(result);
+}); // unicode example
 
-filter("Niagara is beautiful!!!", target)
-.then(result => {if (result) console.log(result)}) // works with sentances btw
+filter('Niagara is beautiful!!!', target).then(result => {
+  if (result) console.log(result);
+}); // works with sentances btw
 
-filter("n.i.a.g.a.r.a", target)
-.then(result => {if (result) console.log(result)}) // someone really trying example
+filter('n.i.a.g.a.r.a', target).then(result => {
+  if (result) console.log(result);
+}); // someone really trying example
 
-filter("n  @#$@i#@$ a   !$#%^#   g#@$a@#r #$@#  a", target)
-.then(result => {if (result) console.log(result)}) // someone really trying example
+filter('n  @#$@i#@$ a   !$#%^#   g#@$a@#r #$@#  a', target).then(result => {
+  if (result) console.log(result);
+}); // someone really trying example
 
-filter("nagra", target)
-.then(result => {if (result) console.log(result)}) // simularity example
+filter('nagra', target).then(result => {
+  if (result) console.log(result);
+}); // simularity example
 
-filter("neagra", target)
-.then(result => {if (result) console.log(result)}) // "sounds like" example
+filter('neagra', target).then(result => {
+  if (result) console.log(result);
+}); // "sounds like" example
 
 //negative result
-filter("The waterfall is beautiful!!!", target)
-.then(result => {if (result) {console.log(result)} else {console.log("All good here")}} )
+filter('The waterfall is beautiful!!!', target).then(result => {
+  if (result) {
+    console.log(result);
+  } else {
+    console.log('All good here');
+  }
+});
